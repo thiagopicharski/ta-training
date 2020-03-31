08ff7601c74c1c303b512732cef84c2584f27957
Lodek <23224552+Lodek@users.noreply.github.com>
2020-03-31 09:32:07 -0300

    test for story 2

--- first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
+++ first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
@@ -1,28 +1,32 @@
 package com.wipro.ta.steps;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.wipro.ta.LikeToTimeDishes;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class MySteps {
 
     private final Gson gson = new Gson();
+    private LikeToTimeDishes.Solution solution;
 
-    private List<Integer> parseList(String listJson){
+    private int[] parseList(String listJson){
         ArrayList<Double> list = gson.fromJson(listJson, ArrayList.class);
-        return list.stream().map(Double::intValue).collect(Collectors.toList());
+        return list.stream().mapToInt(Double::intValue).toArray();
     }
 
-    private List<Integer> dishes;
+    private final LikeToTimeDishes likeToTimeDishes = new LikeToTimeDishes();
+
+    private int[] dishes;
 
     @Given("some $dishes")
     public void parseDishes(@Named("dishes") String dishes){
@@ -30,16 +34,29 @@ public class MySteps {
         this.dishes = parseList(dishes);
     }
 
+    @When("I calculate coefficient")
+    public void getSolution(){
+        this.solution = likeToTimeDishes.calculateSolution(this.dishes);
+        System.out.println(String.format("Calculated solution: %s", this.solution));
+    }
+    @Then("coefficient is $maximum")
+    public void coefficientIsMax(@Named("maximum") int max){
+        assertEquals(max, solution.getCoefficient());
+    }
+
+
     @Given("a solution")
     public void solutionForDish(){
-
-        System.out.println("uwu");
+        this.solution = this.likeToTimeDishes.calculateSolution(this.dishes);
+        System.out.println(String.format("Solution = %s", solution));
     }
+
     @When("I remove a $dish")
     public void removeDish(){
         System.out.println("uwu");
 
     }
+
     @Then("I get a new coefficient")
     public void thenNewCoefficient(){
         System.out.println("uwu");
