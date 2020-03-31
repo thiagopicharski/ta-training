cc58b5398878f086e4cfec9fd3aafb0607f48387
Renan Kohler (Retail) <re40044402@wipro.com>
2020-03-31 16:40:34 -0300

    first commit

---
+++ first-challenge/src/main/java/com/wipro/ta/data/ListDish.java
@@ -0,0 +1,16 @@
+package com.wipro.ta.data;
+
+import java.util.List;
+
+public class ListDish {
+
+    private  int[] list;
+
+    public int[] getList() {
+        return list;
+    }
+
+    public void setList(int[] list) {
+        this.list = list;
+    }
+}
