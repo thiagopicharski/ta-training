b55623424ddf1e4de507db2032a08fc1451e7209
Lodek <23224552+Lodek@users.noreply.github.com>
2020-03-31 09:13:00 -0300

    list conversion and given dishes

--- first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
+++ first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
@@ -1,22 +1,41 @@
 package com.wipro.ta.steps;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class MySteps {
 
-    @Given("some 'dishes'")
-    public void parseDishes(){
-        System.out.println("uwu");
+    private final Gson gson = new Gson();
+
+    private List<Integer> parseList(String listJson){
+        ArrayList<Double> list = gson.fromJson(listJson, ArrayList.class);
+        return list.stream().map(Double::intValue).collect(Collectors.toList());
     }
+
+    private List<Integer> dishes;
+
+    @Given("some $dishes")
+    public void parseDishes(@Named("dishes") String dishes){
+        System.out.println(String.format("Given dishes: %s", dishes));
+        this.dishes = parseList(dishes);
+    }
+
     @Given("a solution")
     public void solutionForDish(){
 
         System.out.println("uwu");
     }
-    @When("I remove a 'dish'")
+    @When("I remove a $dish")
     public void removeDish(){
         System.out.println("uwu");
 
