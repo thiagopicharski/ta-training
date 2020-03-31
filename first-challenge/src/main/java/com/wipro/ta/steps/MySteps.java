c3e09e29c8c7d0de0248fec371255fe2732552b7
Bruno_Santos <brcobal08@gmail.com>
2020-03-31 16:44:07 -0300

    First commit, JBehave

---
+++ first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
@@ -0,0 +1,59 @@
+package com.wipro.ta.steps;
+
+import com.google.gson.Gson;
+import com.wipro.ta.LikeToTimeDishes;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MySteps {
+
+    private static Gson GSON = new Gson();
+    private Map<String, Object> context = new HashMap<String, Object>();
+    private static com.wipro.ta.LikeToTimeDishes LikeToTimeDishes = new LikeToTimeDishes();
+
+
+    @Given("a collection of dishes '$list'")
+    public void givenCollection(@Value("list") String list) {
+        ArrayList<Double> listGson = GSON.fromJson(list, ArrayList.class);
+        int[] listTheDishs = listGson.stream().mapToInt(Double::intValue).toArray();
+
+        context.put("listDish", listTheDishs);
+
+    }
+
+    @When("evaluate coefficient of the dishes")
+    public void evaluatedCoefficient() {
+        int[] listDishs = (int[]) context.get("listDish");
+        LikeToTimeDishes.Solution solution = LikeToTimeDishes.calculateSolution(listDishs);
+        int coefficient = solution.getCoefficient();
+        context.put("actualCoefficient", coefficient);
+
+
+    }
+
+    @Then("the coefficient of the list should be equals '$coefficient'")
+    public void checkCoefficient(@Value("coefficient") int coefficient) {
+
+        int actualCoefficient = (int) context.get("actualCoefficient");
+        Assert.assertEquals(coefficient, actualCoefficient);
+    }
+
+    @When("i remove '$removeDishes'")
+    public void removeDishes(@Value("removeDishes") String removeDishes) {
+        ArrayList<Double> list = GSON.fromJson(removeDishes, ArrayList.class);
+        int[] arrayOfRemovedDishes = list.stream().mapToInt(Double::intValue).toArray();
+        List<Integer> listOfRemovedDishes = Arrays.stream(arrayOfRemovedDishes).boxed().collect(Collectors.toList());
+        int[] arrayDishes = (int[]) context.get("listDish");
+        List<Integer> listWithoutRemovedDishes = Arrays.stream(arrayDishes).boxed().collect(Collectors.toList());
+        listWithoutRemovedDishes.removeAll(listOfRemovedDishes);
+        int[] arrayDishesWithoutRemovedDishes = listWithoutRemovedDishes.stream().mapToInt(i -> i).toArray();
+        context.put("listDish", arrayDishesWithoutRemovedDishes);
+
+    }
+}
---
+++ first-challenge/target/classes/com/wipro/ta/stories/my.story
@@ -0,0 +1,27 @@
+Narrative:
+
+As a user,
+As a user, I want to the total Like-to-time coefficient to be maximum for the dishes.
+
+Scenario: Check that solution returns the correct coefficient for a valid input of dishes.
+
+Given a collection of dishes '<list>'
+When evaluate coefficient of the dishes
+Then the coefficient of the list should be equals '<coefficient>'
+
+Examples:
+
+|list        |coefficient  |
+|[-1,3,4]    |17           |
+
+
+Scenario: Check the solution when a dish must be removed and when anyone is removed
+
+Given a collection of dishes '<list>'
+When i remove '<removeDishes>'
+When evaluate coefficient of the dishes
+Then the coefficient of the list should be equals '<coefficient>'
+
+Examples:
+| list           | removeDishes  | coefficient |
+| [-1,-9,0,5,-7] | [-9, -7]| 14          |
