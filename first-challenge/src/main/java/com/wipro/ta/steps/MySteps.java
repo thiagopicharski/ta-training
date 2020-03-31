93465d3ce6dbc7d98ac9c502862465e323a9d939
Lodek <23224552+Lodek@users.noreply.github.com>
2020-03-31 08:37:07 -0300

    initial step mapping

--- first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
+++ first-challenge/src/main/java/com/wipro/ta/steps/MySteps.java
@@ -6,5 +6,26 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
 public class MySteps {
-    
+
+    @Given("some 'dishes'")
+    public void parseDishes(){
+        System.out.println("uwu");
+    }
+    @Given("a solution")
+    public void solutionForDish(){
+
+        System.out.println("uwu");
+    }
+    @When("I remove a 'dish'")
+    public void removeDish(){
+        System.out.println("uwu");
+
+    }
+    @Then("I get a new coefficient")
+    public void thenNewCoefficient(){
+        System.out.println("uwu");
+
+    }
+
+
 }
