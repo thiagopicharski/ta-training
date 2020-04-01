2dcc2ad82c4dd3cf60db8473876f26a6ba68fa8d
Leonardo Levandoski (Retail) <le40044430@wipro.com>
2020-04-01 14:34:13 -0300

    commit2

--- src/main/java/com/wipro/ta/steps/MySteps.java
+++ src/main/java/com/wipro/ta/steps/MySteps.java
@@ -17,30 +17,29 @@ public class MySteps {
 	
 	public static Gson GSON = new Gson();
 	public Map<String, Object> context = new HashMap<String, Object>();
-//	private static com.wipro.ta.LikeToTimeDishes LikeToTimeDishes = new com.wipro.ta.LikeToTimeDishes();
+	//private static com.wipro.ta.LikeToTimeDishes LikeToTimeDishes = new com.wipro.ta.LikeToTimeDishes();
 	
 	
 	@Given ("a collection '<notRemoved>'")
 	public void givenCollection (@Value("notRemoved") String notRemoved) {
-		String json = "{list:\"" + list + "\"}";
+		
+		
+		String json = "{list:\"" + notRemoved + "\"}";
 		Lista lista = GSON.fromJson(json, Lista.class);
 		
 		context.put("lista", lista);
 		
+				
 	}
 	
-	@When ("calculate this collection by maximum sum of all possible '<list>'")
-	public void whenCalculate(@Value("list") String list) {
+	@When ("calculate this collection by maximum sum of all possible")
+	public void whenCalculate() {
 		
-		ArrayList<Integer> vect = new ArrayList<Integer>();
-		double[] vects = new double [3];
-		
-		for(int i=0; i<3; i++) {
-			
-			
+		List<Integer> notRemoved = new ArrayList<Integer>();
+		for (Integer x : notRemoved) {
+			System.out.println(x);
 		}
-		
-		
+					
 		
 	}
 	
