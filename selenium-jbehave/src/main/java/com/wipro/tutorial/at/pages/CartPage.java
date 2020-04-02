5520e57348c8b7061bb77763577fb224e14b0e08
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-02 16:19:53 -0300

    cart steps

--- selenium-jbehave/src/main/java/com/wipro/tutorial/at/pages/CartPage.java
+++ selenium-jbehave/src/main/java/com/wipro/tutorial/at/pages/CartPage.java
@@ -6,14 +6,16 @@ import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CartPage extends AbstractPage{
 
      private final String CART_ITEM_TEXT_CSS = ".cart-congrats__item-title";
 
-     public List<WebElement> getCartItems(){
-          return webDriverProvider.get().findElements(By.className(CART_ITEM_TEXT_CSS));
+     public List<String> getCartItems(){
+         List<WebElement> cartElements = webDriverProvider.get().findElements(By.className(CART_ITEM_TEXT_CSS));
+         return cartElements.stream().map(WebElement::getText).collect(Collectors.toList());
      }
 
 
