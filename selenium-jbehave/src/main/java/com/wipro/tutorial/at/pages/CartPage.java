61b945f5319280654c5bc7c5be2c7296e4bc114a
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-03 08:32:14 -0300

    removed lambda

--- selenium-jbehave/src/main/java/com/wipro/tutorial/at/pages/CartPage.java
+++ selenium-jbehave/src/main/java/com/wipro/tutorial/at/pages/CartPage.java
@@ -1,10 +1,10 @@
 package com.wipro.tutorial.at.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +15,10 @@ public class CartPage extends AbstractPage{
 
      public List<String> getCartItems(){
          List<WebElement> cartElements = webDriverProvider.get().findElements(By.className(CART_ITEM_TEXT_CSS));
-         return cartElements.stream().map(WebElement::getText).collect(Collectors.toList());
+         List<String> texts = new ArrayList<>();
+         for (WebElement element : cartElements)
+             texts.add(element.getText());
+         return texts;
      }
-
-
 
 }
