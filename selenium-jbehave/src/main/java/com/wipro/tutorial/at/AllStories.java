9eacedf41ded0ba2576f8e674be21e704925c48d
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-03 08:32:27 -0300

    adde runner

--- selenium-jbehave/src/main/java/com/wipro/tutorial/at/AllStories.java
+++ selenium-jbehave/src/main/java/com/wipro/tutorial/at/AllStories.java
@@ -3,14 +3,17 @@ package com.wipro.tutorial.at;
 
 import java.util.List;
 
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.StoryFinder;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.wipro.tutorial.at.configuration.AbstractStoryConfiguration;
 import com.wipro.tutorial.at.configuration.ProjectConfiguration;
 
+@RunWith(JUnitReportingRunner.class)
 public class AllStories extends AbstractStoryConfiguration {
 
 	@Override
@@ -21,7 +24,7 @@ public class AllStories extends AbstractStoryConfiguration {
 	@Override
 	public List<String> storyPaths() {
 		List<String> paths = new StoryFinder().findPaths(
-				CodeLocations.codeLocationFromClass(this.getClass()), "**/stories/searchMercadoLivre.story", "");
+				CodeLocations.codeLocationFromClass(this.getClass()), "**.story", "");
 		return paths;
 	}
 }
