39b0bc212ff27771951b2cb94c69b349b04533bb
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-06 15:58:19 -0300

    steps item remove test

--- third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
+++ third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
@@ -12,6 +12,10 @@ import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Component
 public class CartSteps extends AbstractSteps {
 
@@ -25,6 +29,7 @@ public class CartSteps extends AbstractSteps {
     private EntityUtils entityUtils;
 
     private String response;
+    private int cartId;
 
     @Given("no cart")
     public void noop(){
@@ -41,9 +46,29 @@ public class CartSteps extends AbstractSteps {
 
     @Then("I should get a cart with a single product in it")
     public void cartHasProduct(){
-        DocumentContext cart = jsonUtil.loadJson(cache.getResponseBody());
+        DocumentContext cart = jsonUtil.loadJson(this.response);
         Assert.assertEquals(new Integer(1), cart.read("$.products.length()", Integer.class));
     }
 
+
+    @Given("cart with populated with the following product ids: '$productIds'")
+    public void createCartWithProducts(@Named("productIds") List<Integer> productIds){
+        List<String> productsJson = productIds.stream().map(productOperations::getProduct).collect(Collectors.toList());
+        String response = cartOperations.createCart(productsJson.get(0));
+        this.cartId = entityUtils.getCartId(response);
+        productsJson.subList(1, productsJson.size()).forEach((product) -> cartOperations.addProduct(product, cartId));
+    }
+
+    @When("I remove the product with id '$id' from the cart")
+    public void removeItem(@Named("id") int productId){
+        this.response = cartOperations.removeItem(productId, cartId);
+    }
+
+    @Then("cart no longer contains product '$id'")
+    public void assertCartDoesNotContain(@Named("id") int id){
+        DocumentContext json = jsonUtil.loadJson(this.response);
+        List<Map<String, Object>> matches = json.read("$.products[@.id == " + id, List.class);
+        Assert.assertTrue("The removed item shouldn't be in the cart", matches.size() == 0);
+    }
 
 }
