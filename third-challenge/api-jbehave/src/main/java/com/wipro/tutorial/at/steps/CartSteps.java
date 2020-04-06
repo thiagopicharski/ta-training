420ce9783414f387408400326cfbd84b1cbee082
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-06 15:33:34 -0300

    refactored cart steps

--- third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
+++ third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
@@ -1,8 +1,13 @@
 package com.wipro.tutorial.at.steps;
 
 import com.jayway.jsonpath.DocumentContext;
+import com.wipro.tutorial.at.entities.CartOperations;
+import com.wipro.tutorial.at.entities.EntityUtils;
+import com.wipro.tutorial.at.entities.ProductOperations;
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,22 +16,34 @@ import org.springframework.stereotype.Component;
 public class CartSteps extends AbstractSteps {
 
     @Autowired
-    Cache cache;
+    private ProductOperations productOperations;
 
-    private DocumentContext product;
+    @Autowired
+    private CartOperations cartOperations;
 
-    private String resultBody;
+    @Autowired
+    private EntityUtils entityUtils;
 
+    private String response;
 
     @Given("no cart")
-    public void nop(){
+    public void noop(){
 
     }
+
+
+    @When("I add '$product' to a cart's products")
+    public void addProduct(@Named("product") String productJson) {
+        LOGGER.info("Adding product to new cart");
+        this.response = cartOperations.createCart(productJson);
+    }
+
 
     @Then("I should get a cart with a single product in it")
     public void cartHasProduct(){
         DocumentContext cart = jsonUtil.loadJson(cache.getResponseBody());
         Assert.assertEquals(new Integer(1), cart.read("$.products.length()", Integer.class));
     }
+
 
 }
