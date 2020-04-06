12d4b2e55ce6d777054ead71381f7d0f07e66682
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-06 15:33:59 -0300

    entity utils

---
+++ third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/entities/EntityUtils.java
@@ -0,0 +1,16 @@
+package com.wipro.tutorial.at.entities;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.wipro.tutorial.at.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EntityUtils {
+
+    @Autowired
+    JsonUtil jsonUtil;
+
+    public int getCartId(String cartJson){
+        DocumentContext json = jsonUtil.loadJson(cartJson);
+        return json.read("$.id", Integer.class);
+    }
+}
