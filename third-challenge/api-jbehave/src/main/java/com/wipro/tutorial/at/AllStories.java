24d89f5655e68850886bbf3611cab2e83ca326bb
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-06 14:24:39 -0300

    updated story file in configs

--- third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/AllStories.java
+++ third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/AllStories.java
@@ -1,16 +1,18 @@
 
 package com.wipro.tutorial.at;
 
-import java.util.List;
-
+import com.wipro.tutorial.at.configuration.AbstractStoryConfiguration;
+import com.wipro.tutorial.at.configuration.ProjectConfiguration;
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.StoryFinder;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.wipro.tutorial.at.configuration.AbstractStoryConfiguration;
-import com.wipro.tutorial.at.configuration.ProjectConfiguration;
+import java.util.List;
 
+@RunWith(JUnitReportingRunner.class)
 public class AllStories extends AbstractStoryConfiguration {
 
 	@Override
@@ -21,7 +23,7 @@ public class AllStories extends AbstractStoryConfiguration {
 	@Override
 	public List<String> storyPaths() {
 		List<String> paths = new StoryFinder().findPaths(
-				CodeLocations.codeLocationFromClass(this.getClass()), "**/stories/Hero.story", "");
+				CodeLocations.codeLocationFromClass(this.getClass()), "**/stories/Cart.story", "");
 		return paths;
 	}
 }
