b0be98c4ed9f2d00b40d4af7f9c0e8bdb993f6a9
João Gonçalves <joaovitordutrag@gmail.com>
2020-04-06 11:23:40 -0300

    First challenge first commit

--- third-challenge/ta-training-api/src/main/java/org/example/tatrainingapi/service/CorreiosService.java
+++
@@ -1,9 +0,0 @@
-package org.example.tatrainingapi.service;
-
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-
-public class CorreiosService extends WebServiceGatewaySupport {
-    public Object getPrecoPrazo(String url, Object request){
-        return getWebServiceTemplate().marshalSendAndReceive(url, request);
-    }
-}
