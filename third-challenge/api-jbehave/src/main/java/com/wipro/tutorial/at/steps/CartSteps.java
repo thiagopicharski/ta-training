18ca996f4e393cef10e243318dcebf4122958c50
GregoriSimei <gregorisimei@gmail.com>
2020-04-09 13:38:37 -0300

    Adjusted logger messages and asserts created

--- third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
+++ third-challenge/api-jbehave/src/main/java/com/wipro/tutorial/at/steps/CartSteps.java
@@ -1,10 +1,12 @@
 package com.wipro.tutorial.at.steps;
 
 import com.jayway.jsonpath.DocumentContext;
+import com.wipro.tutorial.at.util.RestUtil;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,30 +18,40 @@ public class CartSteps extends AbstractSteps {
 
     @Given("I a model '$template'")
     public void givenAModel(@Named("template") String template) {
-        DocumentContext json =  jsonUtil.loadJson(template);
-        context.saveResource("payloadCart", json);
+        DocumentContext json = jsonUtil.loadJson(template);
+        context.saveResource("payloadTemplate", json);
+        LOGGER.info("TEMPLATE_JSON: " + json.jsonString());
     }
 
     @Given("a product '$product'")
     public void givenAProduct(@Named("product") String product) {
-        DocumentContext json = (DocumentContext)context.getResource("payloadCart");
-        System.out.println(json.toString() + "   ====================        ================       ==============");
-        json.jsonString();
+        DocumentContext json = jsonUtil.loadJson(product);
+        context.saveResource("jsonProductCreated", json);
+        LOGGER.info("PRODUCT_CREATED: " + json.jsonString());
     }
 
     @Given("I put it on the cart")
     public void putOnTheCart() {
-
+        DocumentContext json = (DocumentContext)context.getResource("jsonProductCreated");
+        String restResult = RestUtil.sendPost(appUrl + "/cart/product", json.jsonString());
     }
 
     @When("I get the product by '$id'")
     public void getProductById(@Named("id") int id) {
-        System.out.println(id + " =======   ID    =======");
+        String restResult = RestUtil.sendGet(appUrl + "/product" + id);
+        DocumentContext json = jsonUtil.loadJson(restResult);
+        context.saveResource("jsonGet", json);
+        DocumentContext jsonProductCreated = (DocumentContext)context.getResource("jsonProductCreated");
+        LOGGER.info("PRODUCT_GET: " + json.jsonString());
+        Assert.assertEquals(json.jsonString(), jsonProductCreated.jsonString());
     }
 
     @Then("I want se '$result'")
-    public void checkGetResult(@Named("result") String product) {
-        System.out.println(product + " ===================    Product    ============");
+    public void checkGetResult(@Named("result") String result) {
+        String jsonResult = jsonUtil.loadJson(result).jsonString();
+        DocumentContext jsonGet = (DocumentContext)context.getResource("jsonGet");
+        LOGGER.info("RESULT: " + jsonResult);
+        Assert.assertEquals(jsonResult, jsonGet.jsonString());
     }
 
 }
--- third-challenge/api-jbehave/src/main/resources/stories/Cart.story
+++ third-challenge/api-jbehave/src/main/resources/stories/Cart.story
@@ -8,5 +8,5 @@ And I put it on the cart
 When I get the product by '<id>'
 Then I want se '<result>'
 Examples:
-| product                                                        | id | result                                                         |
-| { "description": "Batata", "id": 1, "value": 10, "weight": 5 } | 1  | { "description": "Batata", "id": 1, "value": 10, "weight": 5 } |
+| product                                               | id | result                                                 |
+| {"description":"Batata","id":1,"value":10,"weight":5} | 1  | {"description":"Batata","id": 1,"value":10,"weight":5} |
